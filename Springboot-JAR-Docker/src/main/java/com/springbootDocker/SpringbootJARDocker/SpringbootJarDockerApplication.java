fddd20eb0bc9ae9a2c97fcf1b2d34fd29fa44b7f
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-04-07 11:34:45 -0500

    First Commit

---
+++ Springboot-JAR-Docker/src/main/java/com/springbootDocker/SpringbootJARDocker/SpringbootJarDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springbootDocker.SpringbootJARDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJarDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJarDockerApplication.class, args);
+	}
+
+}
---
+++ Springboot-JAR-Docker/src/test/java/com/springbootDocker/SpringbootJARDocker/SpringbootJarDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springbootDocker.SpringbootJARDocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJarDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
